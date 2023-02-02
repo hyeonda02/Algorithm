d193419c6039d2c7159a7bd2f21793d7e24139c3
hyeonda02 <113489721+hyeonda02@users.noreply.github.com>
2023-02-02 23:53:11 +0900

    [Silver III] Title: 문자열 집합, Time: 404 ms, Memory: 31212 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int N;
+	static int M;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		Map<String,Integer> map = new HashMap<>();
+		for(int i=0;i<N;i++) {
+			map.put(br.readLine(),0);
+		}
+		int count =0;
+		for(int i=0;i<M;i++) {
+			if(map.containsKey(br.readLine())) count++;
+		}
+		System.out.println(count);
+		
+		
+	}
+}
