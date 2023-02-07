beb9dbdbb684060145753eefceb0a4704fb4bc68
hyeonda02 <113489721+hyeonda02@users.noreply.github.com>
2023-02-07 23:39:42 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(true){
+            //가지고 있는 빈 병의 개수가 a개보다 미만이면 return
+            if(n<a) break;
+            //가지고 있는 빈 병 수가 a개보다 크면 시작
+            answer+=(n/a)*b;
+            int remain = n%a;
+            
+            n = (n/a)*b + remain;
+                
+        }
+        return answer;
+    }
+}
