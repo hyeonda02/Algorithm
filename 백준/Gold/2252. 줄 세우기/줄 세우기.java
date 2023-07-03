ce6e44070263796df70f353ff0dc96517f6ccf74
다현 <113489721+hyeonda02@users.noreply.github.com>
2023-07-03 22:13:22 +0900

    [Gold III] Title: 줄 세우기, Time: 900 ms, Memory: 49956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.nio.Buffer;
+import java.util.*;
 
 public class Main {
     static int N,M,first,second;
@@ -13,9 +11,10 @@ public class Main {
     static ArrayList<ArrayList<Integer>> arr;
     public static void main (String[]args)throws IOException {
     //2252 줄세우기, 위상정렬
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new ArrayList<>();
         queue = new LinkedList<>();
         for(int i=0;i<=N;i++){
@@ -23,8 +22,9 @@ public class Main {
         }
         indegree = new int[N+1];
         for(int i=0;i<M;i++){
-            first = scanner.nextInt();
-            second = scanner.nextInt();
+            st = new StringTokenizer(br.readLine());
+            first = Integer.parseInt(st.nextToken());
+            second = Integer.parseInt(st.nextToken());
             arr.get(first).add(second);
             indegree[second]++;
         }
