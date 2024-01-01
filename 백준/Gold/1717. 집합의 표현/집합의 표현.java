a5d4d2dc555c67862709578f5ab47098fe92712c
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-01-02 01:52:01 +0900

    [Gold V] Title: 집합의 표현, Time: 1396 ms, Memory: 60228 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,45 +1,60 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    //1717 집합 표현하기 0: union 연산, 1:find 연산
-    static int n,m,a,b,question;
-    static int parent[];
-    public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
+    private static int n;
+    private static int m;
+    private static int a;
+    private static int b;
+    private static int[] parent;
+    private static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            parent[b] = a;
+        }
+    }
+    private static int find(int a){
+        if(parent[a] == a){
+            return a;
+        }else{
+            //주의 -> 경로 압축하면 시간 복잡도 감소됨
+            return parent[a] = find(parent[a]);
+        }
+    }
+    private static void isSame(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a==b){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         parent = new int[n+1];
-        for(int i=1;i<n+1;i++){
+        for(int i=1;i<parent.length;i++){
             parent[i] = i;
         }
         for(int i=0;i<m;i++){
-            question = scanner.nextInt();
-            a = scanner.nextInt();
-            b = scanner.nextInt();
-            if(question==0){
-                //union-find 연산 실행
-                union(a,b);
+            st = new StringTokenizer(br.readLine());
+
+            int check = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if(check == 0){
+                union(a, b);
             }else{
-                if(same(a,b)) System.out.println("YES");
-                else System.out.println("NO");
+                isSame(a, b);
             }
         }
-    }
-    private static void union(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a!=b) parent[b]=a;
-
-    }
-    private static int find(int a){
-        if(a==parent[a]) return a;
-        //시간 복잡도 줄이기 위해 경로 압축 실행
-        else return parent[a]=find(parent[a]);
-    }
-    private static boolean same(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a==b) return true;
-        else return false;
     }
 }
