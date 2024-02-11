ee30d2befcd8cde5e0820f4b8b52dab70ae28053
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-02-12 02:23:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.28 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,23 +2,12 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < food.length; i++) {
-            int num = evenNum(food[i]);
-            for (int j = 0; j < (num/2); j++) {
+            for (int j = 0; j < (food[i]/2); j++) {
                 sb.append(i);
             }
         }
-        String leftResult = sb.toString();
-        StringBuffer sBuffer = new StringBuffer(leftResult);
-        String rightResult = sBuffer.reverse().toString();
-        return leftResult + 0 + rightResult;
-    }
-
-    public int evenNum(int number) {
-        if (number % 2 == 0) {
-            return number;
-        }else {
-            return number - 1;
-        }
+        String left = sb.toString();
+        return left + 0 + sb.reverse().toString();
     }
 
 }
