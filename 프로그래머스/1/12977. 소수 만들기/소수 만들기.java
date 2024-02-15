c00133fd2dafc80e50687252ddb888e370eae3f0
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-02-15 18:52:40 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,14 +1,38 @@
-class Solution {
-   public int solution(int[] nums) {
-        int answer = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int target = nums[i] + nums[j] + nums[k];
-                    if(isPrime(target)) answer++;
-                }
+import java.util.ArrayList;
 
+class Solution {
+       static ArrayList<Integer> combList;
+    public int solution(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        combList = new ArrayList<>();
+        int answer = 0;
+        comb(nums, visited, 0, nums.length, 3 );
+        for (int target: combList) {
+            if (isPrime(target)) {
+                answer++;
             }
+        }
+        return answer;
+    }
+    void comb(int[] arr, boolean[] visited, int start, int n, int r) {
+        if (r == 0) {
+            combList.add(print(arr, visited, n));
+            return;
+        }
+        for (int i = start; i < n; i++ ) {
+            visited[i] = true;
+            comb(arr, visited, i + 1, n, r - 1);
+            visited[i] = false;
+        }
+    }
+
+    int print(int[] arr, boolean[] visited, int n) {
+        int answer =0;
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                answer += arr[i];
+            }
+
         }
         return answer;
     }
