56be4dec30e8a02f48c64c1ef9fe9077db8976a7
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-02-17 04:05:57 +0900

    [Bronze I] Title: 최소공배수, Time: 184 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[]args){
-        //p1934 - 최소 공배수
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        for(int i=0;i<T;i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int result = a*b/gcd(a,b);
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int result = A * B / gcd(A, B);
             System.out.println(result);
         }
+        
     }
-    //최대 공약수를 구하는 연산 (유클리드 호제법 이용) -> 일반적으로 gcd 로 정의한다.
-    private static int gcd(int a, int b){
-        if(b==0)
-            return a;
-        else return gcd(b,a%b);
+    private static int gcd(int A, int B ){
+        if(B==0) return A;
+        else return gcd(B, A % B);
     }
 }
