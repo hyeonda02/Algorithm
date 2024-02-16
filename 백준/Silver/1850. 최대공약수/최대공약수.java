7af1d17dd668042a621064b0994e935704a59b01
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-02-17 04:42:16 +0900

    [Silver I] Title: 최대공약수, Time: 364 ms, Memory: 15524 KB -BaekjoonHub

--- 백준/Silver/1850. 최대공약수/최대공약수.java
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -1,22 +1,24 @@
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[]args)throws Exception{
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
-        long result = gcd(a,b);
-        for(int i=0;i<result;i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long result = gcd(A, B);
+        for (int i = 0; i < result; i++) {
             bw.write("1");
         }
         bw.flush();
         bw.close();
+
     }
-    private static long gcd(long a, long b){
-        if(b==0) return a;
-        else return gcd(b,a%b);
+
+    private static long gcd(long a, long b) {
+        if (b==0) return a;
+        else return gcd(b, a % b);
     }
 }
