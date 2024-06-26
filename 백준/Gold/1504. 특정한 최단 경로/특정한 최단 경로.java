4021869282439d956d5dba2ed2f7121b2ecb6720
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-06-27 02:24:15 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 704 ms, Memory: 69656 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -4,105 +4,102 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-import static java.lang.System.exit;
+//특정한 최단 경로
 
+/**
+ * < 조건 정리 >
+ * 한번 이동했던 정점은 물론, 한번 이동했던 간선도 다시 이동 가능
+ * 1번 정점에서 N번 정점으로 최단 거리로 이동
+ * 그러나, 두 정점은 무조건 통과해야됨
+ * N,E ( 정점, 간선 )
+ * essential1, essential2 필수로 방문해야되는 정점
+ */
 public class Main {
+    static ArrayList<Node>[] graph;
+    static boolean[] visited;
+    static int[] distance;
+    static final int INF = 100000000;
     static int N;
     static int E;
     static int essential1;
     static int essential2;
-
-    static final int INF = 100000000;
-
-    static ArrayList<ArrayList<Node1>> graph;
-    static boolean[] visited;
-
-    static int distance[];
-    static class Node1 implements Comparable<Node1>{
+    static class Node implements Comparable<Node>{
         int index;
         int cost;
-        public Node1(int index, int cost){
-            this.index=index;
-            this.cost=cost;
+
+        public Node(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Node1 o) {
-            return cost-o.cost;
-
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
         }
     }
-    public static int dijkstra(int start, int end){
-        PriorityQueue<Node1> pq= new PriorityQueue<>();
-        Arrays.fill(distance,INF);
-        Arrays.fill(visited,false);
-        pq.offer(new Node1(start,0));
-        distance[start]=0;
-        while(!pq.isEmpty()){
+    static int dijkstraPQ(int start, int end){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Arrays.fill(distance, INF);
+        Arrays.fill(visited, false);
+        pq.offer(new Node(start, 0));
+        distance[start] =0;
+        while (!pq.isEmpty()) {
             int nowVertex = pq.poll().index;
             if(visited[nowVertex]) continue;
-            visited[nowVertex]=true;
-
-            for(Node1 next: graph.get(nowVertex)){
-                if(distance[next.index]>distance[nowVertex]+ next.cost) {
-                    distance[next.index] = distance[nowVertex] + next.cost;
-                    pq.offer(new Node1(next.index,distance[next.index]));
+            visited[nowVertex] = true;
+            for (Node next : graph[nowVertex]) {
+                if (distance[next.index] > distance[nowVertex] + next.cost) {
+                    distance[next.index] = distance[nowVertex]+next.cost;
+                    pq.offer(new Node(next.index, distance[next.index]));
                 }
             }
-
         }
         return distance[end];
-
     }
-    public static void main(String[]args)throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-
-        graph = new ArrayList<>();
         distance = new int[N+1];
-        visited=new boolean[N+1];
+        visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-        Arrays.fill(distance, INF);
 
-        for(int i=0;i<N+1;i++) graph.add(new ArrayList<>());
-
-        for(int i=0;i<E;i++){
-            st=new StringTokenizer(br.readLine());
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            // start에서 end로 가는 weight (가중치)
-            graph.get(s).add(new Node1(e, w));
-
-            // end에서 start로 가는 weight (가중치)
-            graph.get(e).add(new Node1(s, w));
+            graph[s].add(new Node(e, w));
+            graph[e].add(new Node(s, w));
         }
         st = new StringTokenizer(br.readLine());
         essential1 = Integer.parseInt(st.nextToken());
         essential2 = Integer.parseInt(st.nextToken());
 
-        int answer1 =0;
-        answer1+=dijkstra(1,essential1);
-        answer1+=dijkstra(essential1,essential2);
-        answer1+=dijkstra(essential2,N);
-
-
+        int answer1 = 0;
+        answer1 += dijkstraPQ(1, essential1);
+        answer1 += dijkstraPQ(essential1, essential2);
+        answer1 += dijkstraPQ(essential2, N);
         int answer2=0;
-        answer2+=dijkstra(1,essential2);
-        answer2+=dijkstra(essential2,essential1);
-        answer2+=dijkstra(essential1,N);
-
-
+        answer2+=dijkstraPQ(1,essential2);
+        answer2+=dijkstraPQ(essential2,essential1);
+        answer2+=dijkstraPQ(essential1,N);
         int ans = (answer1>=INF && answer2>=INF)?-1:Math.min(answer1,answer2);
-        
+
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(ans+"\n");
         bw.flush();
         bw.close();
         br.close();
+
+
 
 
     }
