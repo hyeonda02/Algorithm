c079778795cb512ad1322a32c362894910053864
DaHyeon Kang <113489721+hyeonda02@users.noreply.github.com>
2024-06-28 23:49:01 +0900

    [level 1] Title: 소수 찾기, Time: 42.53 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,29 +1,25 @@
 class Solution {
-        public int solution(int n) {
-        int answer = isPrime(n);
-        return answer;
-    }
-    private int isPrime(int n) {
-        int[] arr = new int[n + 1];
-        int answer = 0;
-        for (int i = 2; i <= n; i++) {
+    public int[] arr;
+    private void isPrimeNum(int target){
+        arr = new int[target+1];
+        for(int i=2;i<=target;i++){
             arr[i] = i;
         }
-        for (int i = 2; i <= n; i++) {
-            if (arr[i] == 0) {
-                continue;
-            }
-            for (int j = i + i; j <= n; j += i) {
-                arr[j] = 0;
+        for(int i=2;i<=target;i++){
+            if(arr[i] ==0) continue;
+            for(int j=i+i;j<=target;j+=i){
+                arr[j]=0;
             }
         }
-        for (int i = 2; i <= n; i++) {
-            if (arr[i] != 0) {
+    }
+    public int solution(int n) {
+        isPrimeNum(n);
+        int answer =0;
+        for(int i=2;i<=n;i++){
+            if(arr[i]!=0){
                 answer++;
             }
         }
         return answer;
-
     }
-    
 }
