50e8c42d551e6b1ad2a4a3df303dca46b754b6fc
DaHyeon <113489721+hyeonda02@users.noreply.github.com>
2024-09-07 17:19:18 +0900

    [level 1] Title: 소수 찾기, Time: 28.82 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,24 +1,18 @@
 class Solution {
-    public int[] arr;
-    private void isPrimeNum(int target){
-        arr = new int[target+1];
-        for(int i=2;i<=target;i++){
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        for (int i = 2; i <= arr.length-1; i++) {
             arr[i] = i;
         }
-        for(int i=2;i<=target;i++){
-            if(arr[i] ==0) continue;
-            for(int j=i+i;j<=target;j+=i){
-                arr[j]=0;
+        for (int i = 2; i <= arr.length - 1; i++) {
+            if(arr[i] == 0) continue;
+            for (int j = i + i; j <= arr.length - 1; j = j + i) {
+                arr[j] = 0;
             }
         }
-    }
-    public int solution(int n) {
-        isPrimeNum(n);
-        int answer =0;
-        for(int i=2;i<=n;i++){
-            if(arr[i]!=0){
-                answer++;
-            }
+        for (int i = 2; i <= arr.length - 1; i++) {
+            if(arr[i]!=0) answer++;
         }
         return answer;
     }
