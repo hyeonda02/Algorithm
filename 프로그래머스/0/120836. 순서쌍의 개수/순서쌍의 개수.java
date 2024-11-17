89b570b868cbc2ce7cbd1bf2398ec73e250546b6
DaHyeon <113489721+hyeonda02@users.noreply.github.com>
2024-11-18 00:48:25 +0900

    [level 0] Title: 순서쌍의 개수, Time: 2.70 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -2,8 +2,8 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
-        double sqrt = Math.sqrt(n);
-        int answer = (int) IntStream.rangeClosed(1, (int) Math.sqrt(n)).filter(i->n%i==0).count()*2;
-        return sqrt == (int) sqrt ? answer-1 : answer;
+        int sqrt = (int) Math.sqrt(n);
+        long answer = (int) IntStream.rangeClosed(1, sqrt).filter( i -> n % i==0 ).count();
+        return (int) (answer * 2 - (sqrt * sqrt == n ? 1 : 0));
     }
 }
