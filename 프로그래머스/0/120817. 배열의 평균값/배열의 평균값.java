2d3c574b62f9dc995a2859ab458bc37abe5914bc
DaHyeon <kdh021022@gmail.com>
2025-09-08 10:25:39 +0900

    [level 0] Title: 배열의 평균값, Time: 1.76 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,9 +1,17 @@
+// 조건
+// 배열의 길이 = 최대 100
+// 원소의 크기 = 최대 1000
+// 결과는 무조건 .5, .0으로 끝남
+
+// 의사코드
+// 1. numbers 요소들 더하기
+// 2. numbers의 길이로 나누기
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        for(int i=0;i<numbers.length;i++){
-            answer+=numbers[i];
-        }
+        answer = Arrays.stream(numbers).sum();
         return answer/numbers.length;
     }
 }
