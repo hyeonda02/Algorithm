f060e36b9209576e1d37c61201312da1a9b10bae
DaHyeon <kdh021022@gmail.com>
2025-09-08 12:44:05 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 4.36 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,15 +1,24 @@
+// 조건
+// 1. String의 길이가 5이고 n의 길이가 3이라면, 3,2개로 잘라야됨
+// 2. 딱 맞게 떨어지는 경우 (배열의 길이가 9이고, n=3) 엔 3,3,3으로 나눠서 리턴하면 됨
+// 1번의 경우 주의
+
+// 의사코드
+// 1. 몇개로 나눠지는지 계산 
+// 2. 나누기 및 반환
+//   2-1. 마지막의 경우 조심하기 - 끝의 크기를 my_str.length()-1로 해야될듯
+
+
+import java.util.stream.IntStream;
+
 class Solution {
     public String[] solution(String my_str, int n) {
-        int size = (my_str.length() + n - 1) / n; 
-        String[] answer = new String[size]; 
-
-        int start = 0; 
-        for (int i = 0; i < size; i++) {
-            int end = Math.min(start + n, my_str.length()); 
-            answer[i] = my_str.substring(start, end); 
-            start += n; 
-        }
-
-        return answer;
+        // 1. 몇개로 나눠지는지 계산 
+        int size = (my_str.length()+n-1)/n;
+        // 2. 그 개수만큼 돌면서 자르기(나누기 및 반환)
+        return IntStream.range(0,size)
+            .mapToObj(i -> my_str.substring(i*n, Math.min((i+1)*n, my_str.length())))
+            .toArray(String[]::new);
+        
     }
 }
