7fc59d1bff3f0b5a5e27bd18ce96b1934f842127
DaHyeon <kdh021022@gmail.com>
2026-01-25 01:31:02 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.46 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%2==1) list.add(i);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+    }
+}
