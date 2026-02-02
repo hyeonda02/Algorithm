12091306592592a1db28aafa322094231fcbd50d
DaHyeon <kdh021022@gmail.com>
2026-02-03 00:58:17 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.73 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        // String의 길이가 1000밖에 안되서 TreeMap 사용 -> 정렬 보장
+        Map<Character, Integer> map = new TreeMap<>();
 
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        List<Character> list = new ArrayList<>();
-        for (char key : map.keySet()) {
-            if (map.get(key) == 1) {
-                list.add(key);
+        
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            if(entry.getValue()==1){
+                sb.append(entry.getKey());
             }
         }
-
-        Collections.sort(list);
-
-        StringBuilder sb = new StringBuilder();
-        for (char c : list) {
-            sb.append(c);
-        }
-
         return sb.toString();
     }
 }
