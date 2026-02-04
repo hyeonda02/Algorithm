266168b2a0602623173c53d15869605a6533e37c
DaHyeon <kdh021022@gmail.com>
2026-02-04 16:52:24 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.36 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        char[] charArr = answer.toCharArray();
+        Arrays.sort(charArr);
+        return String.valueOf(charArr);
+    }
+}
