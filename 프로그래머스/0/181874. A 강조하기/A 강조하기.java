1297e5c3142c3686f18e623792a9d40bac1b6787
DaHyeon <kdh021022@gmail.com>
2026-02-09 10:25:19 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : myString.toCharArray()){
-            if(c=='A'||c=='a') {
-                sb.append(Character.toUpperCase(c));
-            } else {
-                sb.append(Character.toLowerCase(c));
-            }
-        }
-        return sb.toString();
+        myString = myString.toLowerCase();
+        myString = myString.replace('a','A');
+        return myString;
     }
 }
