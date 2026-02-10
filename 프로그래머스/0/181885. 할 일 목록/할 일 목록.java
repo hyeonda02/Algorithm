929e6ba85c62e7a208d5be52f78e02fbcd65057d
DaHyeon <kdh021022@gmail.com>
2026-02-10 11:17:12 +0900

    [level 0] Title: 할 일 목록, Time: 1.90 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for(int i=0;i<finished.length;i++){
+            if(finished[i]==false) answer.add(todo_list[i]);
+        }
+        
+        return answer.stream().toArray(String[]::new);
+        
+    }
+}
