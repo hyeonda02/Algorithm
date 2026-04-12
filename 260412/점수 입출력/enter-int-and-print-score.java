15e1b5728ef3c02441d1d20ad448435f0c90763a
DaHyeon <kdh021022@gmail.com>
2026-04-12 23:36:22 +0900

    Title: 점수 입출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260412/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println("Your score is "+N+" point.");
+    }
+}
