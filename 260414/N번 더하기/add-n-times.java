b9c7393f0409587d6f7c84f07cf2d2cbf0f877c3
DaHyeon <kdh021022@gmail.com>
2026-04-14 22:16:19 +0900

    Title: N번 더하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/N번 더하기/add-n-times.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        for(int i=0;i<N;i++){
+            System.out.println(A+=N);
+        }
+    }
+}
