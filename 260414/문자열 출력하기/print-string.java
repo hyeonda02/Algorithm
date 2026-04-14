61ede087113178c898204d83fe79f3d723a3f618
DaHyeon <kdh021022@gmail.com>
2026-04-14 22:14:39 +0900

    Title: 문자열 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=0;i<N;i++){
+            System.out.println("LeebrosCode");
+        }
+
+    }
+}
