24d6e6a83507c8eec8d0df44921ae62365bbf6af
DaHyeon <kdh021022@gmail.com>
2026-04-14 21:56:07 +0900

    Title: 비교에 따른 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println((a>b)?a*b:b/a);
+
+    }
+}
