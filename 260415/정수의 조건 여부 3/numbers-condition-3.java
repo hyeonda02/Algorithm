fc190dca01e762d08215a7e507ba1dab6a4dfc30
DaHyeon <kdh021022@gmail.com>
2026-04-15 23:05:49 +0900

    Title: 정수의 조건 여부 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a%13==0 || a%19==0){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
