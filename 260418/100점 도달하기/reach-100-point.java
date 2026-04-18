19149e1e6a1942eaddaa16b35d7ff608c1c7a351
DaHyeon <kdh021022@gmail.com>
2026-04-18 15:47:49 +0900

    Title: 100점 도달하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/100점 도달하기/reach-100-point.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.next());
+
+        for(int i = a;i<=100;i++){
+            if(i>=90){System.out.print('A');}
+            else if(i>=80){System.out.print('B');}
+            else if(i>=70){System.out.print('C');}
+            else if(i>=60){System.out.print('D');}
+            else {System.out.print('F');}
+
+            System.out.print(" ");
+        }
+    }
+}
