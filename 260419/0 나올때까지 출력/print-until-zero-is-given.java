95cf5948c6fc997643446c0fbcfbb805d33d5aef
DaHyeon <kdh021022@gmail.com>
2026-04-19 15:57:44 +0900

    Title: 0 나올때까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            int n = scanner.nextInt();
+            if(n==0) {
+                break;
+            }
+            System.out.println(n);
+        }
+        
+    }
+}
