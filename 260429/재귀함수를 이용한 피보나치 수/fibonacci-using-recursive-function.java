4395f0b41f7e7032c7c7b4226e8794af5880b26d
DaHyeon <kdh021022@gmail.com>
2026-04-29 22:58:20 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int fibo(int n){
+        if(n==1||n==2) return 1;
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(fibo(n));
+    }
+}
