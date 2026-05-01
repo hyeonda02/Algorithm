a590ff6dc71c308d8273c980c7b8c2003262dce9
DaHyeon <kdh021022@gmail.com>
2026-05-02 01:13:43 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (121ms, 10MB)

--- 260502/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
+++ 260502/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Main {
-    public static int solution(int n, int answer){
-        if(n==1) return answer;
-        if(n%2==0) return solution(n/2,++answer);
-        return solution(n*3+1,++answer);
+    public static int solution(int n){
+        if(n==1) return 0;
+        if(n%2==0) return solution(n/2)+1;
+        return solution(n*3+1)+1;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
-        System.out.println(solution(n,0));
+        System.out.println(solution(n));
     }
 }
